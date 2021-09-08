dde5ffc46e4754965b6d798a470822bb1ba47e4a
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-09-08 12:54:59 +0200

    'Update'

--- OCRReading_HALCON/scripts/OCRReading.lua
+++ OCRReading_HALCON/scripts/OCRReading.lua
@@ -1,29 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  OCRReading_HALCON
-
-  Summary:
-  Example for OCR reading within AppSpace using the HALCON API.
-
-  Description:
-  This App shows how to use a trained OCR handle to read characters in an image
-  using functionality implemented in HALCON. For running this sample a trained
-  OCR handle is necessary. If the sample OCRTraining_HALCON is run first, its
-  output in public AppData will be used. Otherwise there is a trained handle
-  provided as a resource with this app, which will be used instead. The result
-  of the reading will be shown in the standard 2D viewer.
-
-  How to run:
-  The results can be seen in the 2D viewer on the webpage. Restarting the Sample may
-  be necessary to show images after loading the webpage.
-  To run this Sample a device with SICK Algorithm API, HALCON support and
-  AppEngine >= V2.5.0 is required. E.g. SIM4000 with latest firmware.
-
-  Implementation:
-  This sample is written using HALCON 12.
-
-------------------------------------------------------------------------------]]
 print('AppEngine Version: ' .. Engine.getVersion())
 
 --Start of Function and Event Scope---------------------------------------------
